7ef6a0b878b27914d6b741ccb7eb4a6a5502dcde
elpbatista <elpbatista@gmail.com>
2022-12-18 08:44:26 -0800

    adding stats

--- code/data-collection.js
+++ code/data-collection.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const turf = require("@turf/turf");
+const sm = require("statistical-methods");
 
 const Pts = JSON.parse(
   fs.readFileSync("../data/Ptos_Vereda_Trasdelalto.geojson")
@@ -49,22 +50,55 @@ var su = turf.collect(SU, pts, "AREA_M2", "area");
 var su = turf.collect(SU, pts, "DIRECC_NOR", "dir");
 var su = turf.collect(SU, pts, "fid", "fid");
 
-function Sum(array) {
-  flatten = array.flat(Infinity);
-  return flatten.length > 0 ? flatten.reduce((a, b) => a + b) : 0;
-}
-
 su.features.map((a) => {
-  a.properties.ppl = Sum(a.properties.personas);
+  a.properties.ppl = sm.sum(a.properties.personas.flat(Infinity));
   delete a.properties.personas;
-  a.properties.fam = Sum(a.properties.hogares);
+  a.properties.fam = sm.sum(a.properties.hogares.flat(Infinity));
   delete a.properties.hogares;
-  a.properties.houses = Sum(a.properties.viviendas);
+  a.properties.houses = sm.sum(a.properties.viviendas.flat(Infinity));
   delete a.properties.viviendas;
-  a.properties.t_area = Math.round(Sum(a.properties.area));
+  a.properties.t_area = Math.round(sm.sum(a.properties.area.flat(Infinity)));
   delete a.properties.area;
   a.properties.addrs = a.properties.fid.length;
   delete a.properties.fid;
 });
 
-console.dir(su, { depth: null });
+su.features = su.features.filter((a)=>a.properties.addrs > 0)
+
+/**
+ * Normalization
+ * variation = (range[1] - range [0]) / (max - min)
+ * valN = (range[0] + (val - min) * variation)
+ */
+
+const range = [1, 10];
+const dec = 3;
+const Normalize = (range, min, max, val) =>
+  range[0] + ((val - min) * (range[1] - range[0])) / (max - min);
+
+var pplMax = sm.max(su.features.map((a) => a.properties.ppl));
+var pplMin = sm.min(su.features.map((a) => a.properties.ppl));
+
+var famMax = sm.max(su.features.map((a) => a.properties.fam));
+var famMin = sm.min(su.features.map((a) => a.properties.fam));
+
+var housesMax = sm.max(su.features.map((a) => a.properties.houses));
+var housesMin = sm.min(su.features.map((a) => a.properties.houses));
+
+var t_areaMax = sm.max(su.features.map((a) => a.properties.t_area));
+var t_areaMin = sm.min(su.features.map((a) => a.properties.t_area));
+
+var addrsMax = sm.max(su.features.map((a) => a.properties.addrs));
+var addrsMin = sm.min(su.features.map((a) => a.properties.addrs));
+
+var t_areaNm = su.features.map(
+  (a) => +Normalize(range, t_areaMin, t_areaMax, a.properties.t_area).toFixed(dec)
+  // (a) => Normalize(range, t_areaMin, t_areaMax, a.properties.t_area)
+);
+
+console.dir(
+  su.features.map((a) => a.properties.t_area),
+  { depth: null }
+);
+console.dir(t_areaNm, { depth: null });
+// console.log(pplMax);
