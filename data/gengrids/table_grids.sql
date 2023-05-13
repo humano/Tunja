e8eee35c168cdb1cc2f4972b20d1c070d28625d9
0e1 <0e1@riseup.net>
2023-05-12 22:04:36 -0300

    Update osmcode and create grid.

--- data/gengrids/table_grids.sql
+++ data/gengrids/table_grids.sql
@@ -43,11 +43,25 @@ CREATE TABLE gridstunja.cover1 AS
       ) AS geom
     FROM
     (
-      SELECT bbox, srid, jurisd_base_id, kx_prefix, cindex, codebits, la, isolabel_ext,
-              natcod.vbit_to_strstd( osmc.vbit_from_16h_to_vbit_b32nvu((codebits || la),jurisd_base_id),'32nvu') AS code,
-              ggeohash.draw_cell_bybox(ggeohash.decode_box2(osmc.vbit_withoutL0(codebits,'CO') || la,bbox,false),false,srid) AS geom
-      FROM gridstunja.cover0 c,
-      unnest('{00000,00001,00010,00011,00100,00101,00110,00111,01000,01001,01010,01011,01100,01101,01110,01111,10000,10001,10010,10011,10100,10101,10110,10111,11000,11001,11010,11011,11100,11101,11110,11111}'::varbit[]) d(la)
+      SELECT z.bbox, z.srid, z.jurisd_base_id, z.codebits, z.la, z.isolabel_ext, z.code, z.geom,
+          CASE
+          WHEN x.kx_prefix IS NULL THEN z.kx_prefix
+          ELSE x.kx_prefix
+          END AS kx_prefix,
+          CASE
+          WHEN x.cindex IS NULL THEN z.cindex
+          ELSE x.cindex
+          END AS cindex
+      FROM
+      (
+        SELECT bbox, srid, jurisd_base_id, kx_prefix, cindex, codebits, la, isolabel_ext,
+                natcod.vbit_to_strstd( osmc.vbit_from_16h_to_vbit_b32nvu((codebits || la),jurisd_base_id),'32nvu') AS code,
+                ggeohash.draw_cell_bybox(ggeohash.decode_box2(osmc.vbit_withoutL0(codebits,'CO') || la,bbox,false),false,srid) AS geom
+        FROM gridstunja.cover0 c,
+        unnest('{00000,00001,00010,00011,00100,00101,00110,00111,01000,01001,01010,01011,01100,01101,01110,01111,10000,10001,10010,10011,10100,10101,10110,10111,11000,11001,11010,11011,11100,11101,11110,11111}'::varbit[]) d(la)
+      ) z
+      LEFT JOIN (SELECT * FROM osmc.coverage  WHERE isolabel_ext = 'CO-BOY-Tunja' AND is_overlay IS TRUE) x
+      ON z.code = x.kx_prefix
     ) e
     WHERE
         ST_Intersects(
@@ -64,7 +78,9 @@ CREATE TABLE gridstunja.cover1 AS
 -- Grid L11.5 (181m)
 DROP TABLE IF EXISTS gridstunja.cover2;
 CREATE TABLE gridstunja.cover2 AS
-  SELECT ROW_NUMBER() OVER (ORDER BY code) AS gid, jurisd_base_id, isolabel_ext, kx_prefix, cindex, code, substring(code,1,length(code)-1) AS prefix, substring(code FROM length(code)) AS suffix, codebits, srid, bbox, geom
+  SELECT ROW_NUMBER() OVER (ORDER BY code) AS gid, jurisd_base_id, isolabel_ext, kx_prefix, cindex, code,
+  CASE WHEN length(code) =1 THEN code ELSE substring(code,1,length(code)-1) END AS prefix,
+  CASE WHEN length(code) =1 THEN ''   ELSE substring(code FROM length(code)) END AS suffix, codebits, srid, bbox, geom
   FROM
   (
     SELECT jurisd_base_id, kx_prefix, regexp_replace(code,'^'||kx_prefix,cindex) AS code, codebits || la AS codebits, srid, bbox, isolabel_ext, cindex,
